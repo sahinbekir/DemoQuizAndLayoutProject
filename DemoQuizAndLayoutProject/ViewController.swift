1eb5af82d104e23e3e812de67401faf7ae5cd8c5
sahinbekir <bsbekirsahinbs@gmail.com>
2024-07-09 16:18:59 +0500

    Initial Commit

---
+++ DemoQuizAndLayoutProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoQuizAndLayoutProject
+//
+//  Created by sahinbekir on 9.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
